cffb3927c39d355df6bb28d0deb3416c30bb3bd4
RED <apache_red@mail.ru>
2019-03-17 11:06:31 +0300

    git init

---
+++ src/main/java/com/redcompany/red/hibernate/one/App.java
@@ -0,0 +1,13 @@
+package com.redcompany.red.hibernate.one;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
